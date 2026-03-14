508b44573c578bd2eab5210b5f6ad7e10259180a
modavid <modavid@student.42.fr>
2026-03-14 13:32:45 +0100

    testwp

---
+++ srcs/requirements/mariadb/tools/init-db.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+mysqld_safe &
+
+until mysqladmin ping --silent; do
+    echo "Waiting for MariaDB..."
+    sleep 1
+done
+
+mysql -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
+mysql -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+mysql -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
+mysql -e "FLUSH PRIVILEGES;"
+
+mysqladmin -u root -p$MYSQL_ROOT_PASSWORD shutdown
+
+exec mysqld_safe
---
+++ srcs/requirements/wordpress/tools/wordpress_config.sh
@@ -0,0 +1,44 @@
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+chmod +x wp-cli.phar
+mv wp-cli.phar /usr/local/bin/wp
+
+wait_for_db() {
+    echo "Waiting for database connection for $MYSQL_USER@$MYSQL_PASSWORD"
+    until mysql -h "mariadb" -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" -e "SHOW DATABASES;" > /dev/null 2>&1; do
+        echo "Database is not ready. Retrying in 5 seconds..."
+        sleep 5
+    done
+    echo "Database is ready!"
+}
+
+wait_for_db
+
+wp config create \
+    --dbname=$MYSQL_DATABASE \
+    --dbuser=$MYSQL_USER \
+    --dbpass=$MYSQL_PASSWORD \
+    --dbhost=$WP_MYSQL_HOST \
+    --url="https://$DOMAIN_NAME" \
+    --allow-root
+
+wp core install --url=$DOMAIN_NAME \
+    --title=$WP_TITLE \
+    --admin_user=$WP_ADMIN_USER \
+    --admin_password=$WP_ADMIN_PASSWORD \
+    --admin_email=$WP_ADMIN_EMAIL \
+    --skip-email \
+    --allow-root
+
+wp user create $WP_USER $WP_USER_EMAIL \
+    --user_pass=$WP_USER_PASSWORD \
+    --role=$WP_USER_ROLE \
+    --allow-root
+
+chmod -R 755 /var/www/wordpress/
+chown -R www-data:www-data /var/www/wordpress
+
+echo "WORDPRESS STARTING..."
+
+exec php-fpm8.2 \
+    --nodaemonize \
+    --allow-to-run-as-root
